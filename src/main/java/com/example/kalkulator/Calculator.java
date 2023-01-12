db2bad6dc1ee79d0fea48eabdd2d03cbcbbec66a
Sebastian Wojdal <sebastianwojdal@wp.pl>
2023-01-12 20:50:57 +0100

    Calculator

---
+++ src/main/java/com/example/kalkulator/Calculator.java
@@ -0,0 +1,14 @@
+package com.example.kalkulator;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Calculator {
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractAFromB(int a, int b) {
+        return b - a;
+    }
+}
